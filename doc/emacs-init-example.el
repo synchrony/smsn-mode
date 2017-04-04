3802e72432b0c76be3944a2cf842a26279029bb1
Joshua Shinavier <joshsh@uber.com>
2017-04-04 15:59:41 -0700

    Moved external search shortcuts into init example

--- doc/emacs-init-example.el
+++ doc/emacs-init-example.el
@@ -31,3 +31,27 @@
 
 ;; RDF output is written here
 ;;(defvar smsn-default-rdf-file "/path/to/mygraphdata/graph.nt")
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; additional useful shortcuts
+
+(define-key smsn-mode-map (kbd "C-c C-t C-b a")   (smsn-search-in-browser
+  "Amazon" "http://www.amazon.com/s?ie=UTF8&index=blended&link_code=qs&field-keywords="))
+(define-key smsn-mode-map (kbd "C-c C-t C-b d")   (smsn-search-in-browser
+  "Delicious" "http://www.delicious.com/search?p="))
+(define-key smsn-mode-map (kbd "C-c C-t C-b e")   (smsn-search-in-browser
+  "eBay" "http://www.ebay.com/sch/i.html?_nkw="))
+(define-key smsn-mode-map (kbd "C-c C-t C-b g")   (smsn-search-in-browser
+  "Google" "http://www.google.com/search?ie=UTF-8&q="))
+(define-key smsn-mode-map (kbd "C-c C-t C-b m")   (smsn-search-in-browser
+  "Google Maps" "http://maps.google.com/maps?q="))
+(define-key smsn-mode-map (kbd "C-c C-t C-b s")   (smsn-search-in-browser
+  "Google Scholar" "http://scholar.google.com/scholar?q="))
+(define-key smsn-mode-map (kbd "C-c C-t C-b t")   (smsn-search-in-browser
+  "Twitter" "http://twitter.com/#!/search/"))
+(define-key smsn-mode-map (kbd "C-c C-t C-b w")   (smsn-search-in-browser
+  "Wikipedia" "http://en.wikipedia.org/w/index.php?title=Special%3ASearch&search="))
+(define-key smsn-mode-map (kbd "C-c C-t C-b y")   (smsn-search-in-browser
+  "YouTube" "http://www.youtube.com/results?search_query="))
+
