10b70d142239ccfb25be5d5ce584ea9ac342834d
Joshua Shinavier <joshsh@uber.com>
2017-04-04 09:04:41 -0700

    Simplified example init file

--- doc/smsn-mode-init-example.el
+++ doc/smsn-mode-init-example.el
@@ -1,8 +1,8 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Example init file for use with SmSn-mode
 
-;; copy or link to SmSn-mode's ./lisp directory here
-(let ((default-directory "~/.emacs.d/elisp/"))
+;; relace /path/to/smsn-mode with actual path to the SmSn-mode source directory
+(let ((default-directory "/path/to/smsn-mode"))
       (normal-top-level-add-subdirs-to-load-path))
 
 ;; load the library
@@ -18,16 +18,16 @@
 (defvar smsn-server-protocol "websocket")
 
 ;; export and import graph data in the VCS format from here
-(defvar smsn-default-vcs-file "/mygraphdata/git-smsn")
+;;(defvar smsn-default-vcs-file "/path/to/mygraphdata/git-smsn")
 
 ;; export the graph to here, or populate an empty graph from here
-(defvar smsn-default-graphml-file "/mygraphdata/graph.xml")
+;;(defvar smsn-default-graphml-file "/path/to/mygraphdata/graph.xml")
 
 ;; default location for dumps of tab-separated vertex and edge files
 ;; vertex files contain the properties of each atom
 ;; edge files are parent/child adjacency lists
-(defvar smsn-default-vertices-file "/mygraphdata/vertices.tsv")
-(defvar smsn-default-edges-file "/mygraphdata/edges.tsv")
+;;(defvar smsn-default-vertices-file "/path/to/mygraphdata/vertices.tsv")
+;;(defvar smsn-default-edges-file "/path/to/mygraphdata/edges.tsv")
 
 ;; RDF output is written here
-(defvar smsn-default-rdf-file "/mygraphdata/graph.nt")
+;;(defvar smsn-default-rdf-file "/path/to/mygraphdata/graph.nt")
