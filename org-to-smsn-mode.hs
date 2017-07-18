4b8fcc2578682afce7091356474c74aaaf1d0c7f
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-07-17 21:01:41 -0700

    comments

--- org-to-smsn-mode.hs
+++ org-to-smsn-mode.hs
@@ -1,5 +1,10 @@
--- It expects the input file to consist entirely of foldable org lines.
+-- This expects the input file to consist entirely of foldable org lines.
   -- That is, each line starts with at least one *, followed by a space.
+-- If a line starts with n asterisks, then
+  -- the first n-1 asterisks are each converted to four spaces
+  -- and the last one is left intact, along with the rest of the line.
+-- Any other kind of line will be retained without change.
+
 -- Run it using 'runghc' (part of a standard Haskell installation).
   -- runghc org-to-smsn-mode.hs file.org file.smsn
 
