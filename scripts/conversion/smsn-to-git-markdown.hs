4611d092e913074cb3f9ab8cc834d25187df1267
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-07-24 01:35:52 -0700

    toward counting URL substitutions

--- scripts/conversion/smsn-to-git-markdown.hs
+++ scripts/conversion/smsn-to-git-markdown.hs
@@ -4,7 +4,7 @@
 {-# LANGUAGE ViewPatterns #-}
 
 import Data.List (span, stripPrefix)
-import Data.List.Split (splitWhen)
+import Data.List.Split (splitWhen, splitOn)
 import Data.Maybe
 import System.Environment (getArgs)
 
@@ -34,11 +34,50 @@ countLeadingSpace s = (n, s') where
 stripSmsnAddress :: String -> String
 stripSmsnAddress = drop 21
 
-data Command = File String | Text String | Code String | Url String | Ignore
-  deriving Show
+data Command = File String
+             | Text String
+             | Code String
+             | Url String
+             | Ignore         deriving Show
 
--- countUrlSubstitutions :: String -> UrlCount
--- countUrlSubstitutions s = f s
+countUrlSubstitutions :: String -> UrlCount
+countUrlSubstitutions s = f 0 s where
+  f n (']' : '(' : ')' : s') = f (n+1) s'
+  f n (_:s') = f n s'
+  f n [] = n
+
+interleave :: [String] -> [String] -> String
+interleave outer inner = concat $ tail $ concat -- drops the "discard"
+  $ map (\(a,b) -> [a,b])
+  $ zip ("discard" : inner) outer -- inner is one shorter than outer
+
+substituteUrlsOnce :: String -> [String] -> String
+substituteUrlsOnce s urls =  interleave sDivided urlsBracketed
+    where sDivided = splitOn "]()" s
+          urlsBracketed = map (\url -> "](" ++ url ++ ")") urls
+
+-- this should consume all URLs; if not, the input text has mismatches
+substituteUrls :: [(IndentLevel,Command)] -> [Command]
+substituteUrls levComs = f [] levComs where
+  f :: [Command] -> [(IndentLevel,Command)] -> [Command]
+  f accumulator ((lev, Text s) : rest) = let
+    urlCount = countUrlSubstitutions s
+    sWithSubs = substituteUrlsOnce s
+               $ map (fromUrl . snd)
+               $ take urlCount rest
+    in f (Text sWithSubs : accumulator) $ drop urlCount rest
+  f accumulator ((lev, com) : rest) = f (com : accumulator) rest
+  f accumulator [] = accumulator
+
+substituteUrls_test = substituteUrls
+  [ (2, Text "[frog]() and [goat]() are friends")
+  ,  (3, Url "http://frog")
+  ,  (3, Url "http://goat")
+  ]
+
+fromUrl :: Command -> String
+fromUrl (Url s) = s
+fromUrl _ = error "fromUrl applied to non-URL"
 
 readCommand :: String -> Command
 readCommand (stripPrefix "[markdown]" -> Just restOfLine)
@@ -47,6 +86,8 @@ readCommand (stripPrefix "[markdown-code]" -> Just restOfLine)
   = Code restOfLine
 readCommand (stripPrefix "[target-filename]" -> Just restOfLine)
   = File restOfLine
+readCommand (stripPrefix "[url-leaf]" -> Just restOfLine)
+  = Url restOfLine
 readCommand s = Ignore
 
 markdown :: [Command] -> String
@@ -55,6 +96,7 @@ markdown es = unlines $ mapMaybe f es where
   f (File s) = Nothing
   f (Text s) = Just $ "\n" ++ s
   f (Code s) = Just s
+  f (Url u) = Just $ error "Unusued Url: " ++ u
   f Ignore = Nothing
   -- In markdown, all ordinary text lines need to be preceded by a newline,
   -- but lines of code, including the bracketing ``` lines, do not.
